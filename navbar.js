08c8fe2558d1896f7b4aef1d5188bc4141d76671
crn1 <gluvajic@aol.com>
2023-08-24 00:06:19 +0200

    Added candidates and jobs HTMLs

--- navbar.js
+++ navbar.js
@@ -1,8 +1,12 @@
 let appNavbar = `
   <div class="topnav" id="appTopnav">
     <a title="starmode recruiting" href="./index.html" onclick="javascript:delay('./index.html');return false;" id="topnav-logo-link">starmode <em>recruiting</em></a>
+
     <a title="About Me" href="./about.html" onclick="javascript:delay('./about.html');return false;" id="topnav-first-link">about</a>
     <a title="Blog" href="./blog.html" onclick="javascript:delay('./blog.html');return false;">blog</a>
+    <a title="Jobs" href="./jobs.html" onclick="javascript:delay('./jobs.html');return false;">jobs</a>
+    <a title="Candidates" href="./candidates.html" onclick="javascript:delay('./candidates.html');return false;">candidates</a>
+
     <a href="javascript:void(0);" class="icon" onclick="navbarFunction()">
     <i class="fa fa-bars"></i>
     </a>
