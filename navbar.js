13bf7832e870a8f0e3d18570119a31730cdf8e08
crn1 <gluvajic@aol.com>
2023-08-24 15:32:08 +0200

    Updated jobs.html

--- navbar.js
+++ navbar.js
@@ -5,7 +5,7 @@ let appNavbar = `
     <a title="About Me" href="./about.html" onclick="javascript:delay('./about.html');return false;" id="topnav-first-link">about</a>
     <a title="Blog" href="./blog.html" onclick="javascript:delay('./blog.html');return false;">blog</a>
     <a title="Jobs" href="./jobs.html" onclick="javascript:delay('./jobs.html');return false;">jobs</a>
-    <a title="Candidates" href="./candidates.html" onclick="javascript:delay('./candidates.html');return false;">candidates</a>
+    <!-- <a title="Candidates" href="./candidates.html" onclick="javascript:delay('./candidates.html');return false;">candidates</a> -->
 
     <a href="javascript:void(0);" class="icon" onclick="navbarFunction()">
     <i class="fa fa-bars"></i>
